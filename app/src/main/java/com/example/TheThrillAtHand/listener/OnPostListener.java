de3330769ea271b2d997ef8377f475b95be0394f
ParkSungJin <psjin312@gmail.com>
2020-06-13 21:59:55 +0900

    V0.1 게시판 구현 완료, 회원 정보에서 로그아웃 버튼 추가하기 전

---
+++ app/src/main/java/com/example/TheThrillAtHand/listener/OnPostListener.java
@@ -0,0 +1,8 @@
+package com.example.TheThrillAtHand.listener;
+
+import com.example.TheThrillAtHand.PostInfo;
+
+public interface OnPostListener {
+    void onDelete(PostInfo postInfo);
+    void onModify();
+}
